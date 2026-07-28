dbdee90b0d4967d7c5b846dc2d135e3111e77c53
Sourav Verma <souravverma942@gmail.com>
2026-07-28 16:41:41 +0530

    Day 2: Basic route, GET request, Uvicorn, and Swagger UI

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+# Home Route 
+
 @app.get("/")
 def home():
     return {"message": "Hello World, FastAPI!"}
+
+
+# about route
+@app.get("/about")
+def about():
+    return {"messages":"This is About Page"}
+
+# users Route
+
+@app.get("/users")
+def users():
+    return {
+        "users":["Mohit","Rohit","Sourav"]
+    }

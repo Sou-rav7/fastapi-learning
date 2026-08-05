5f8c50fb6ef4ecb73dd86bcc7d61c944604ed1ef
Sourav Verma <souravverma942@gmail.com>
2026-08-05 23:39:35 +0530

    Day 4: Query parameters, optional parameters and default values

--- main.py
+++ main.py
@@ -2,23 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/users")
+def get_users(name: str = None):
+    return {"name": name}
 
-# Home Route 
-
-@app.get("/")
-def home():
-    return {"message": "Hello World, FastAPI!"}
+@app.get("/products")
+def get_users(limit : int = 10):
+    return {"limit": limit}
 
 
-# about route
-@app.get("/about")
-def about():
-    return {"messages":"This is About Page"}
 
-# users Route
-
-@app.get("/users/{user_id}")
-def get_users(user_id:int):   
-      return {"user_id": user_id}
-    
-
+@app.get("/items")
+def get_users(name: str = None, price: int=10):
+    return {
+        "name": name,
+        "price":price,
+    }
